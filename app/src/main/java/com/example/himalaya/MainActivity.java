81d6ff924efea64615c34685954d723e91bd4ac9
wuyingheng <1595110800@qq.com>
2022-02-02 22:57:09 +0800

    test

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.himalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("hah" ,
+                "ha" );
     }
 }
